ea1e9b1fabe2b13dd1d97baff90d6a1bf1becc2d
Chenxu <chenxu4012@foxmail.com>
2021-06-20 00:29:25 +0800

    update config

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Chenxu
  * @Date: 2021-03-23 10:47:00
- * @LastEditTime: 2021-03-26 15:06:15
+ * @LastEditTime: 2021-06-20 00:22:44
  * @Msg: Nothing
  */
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
@@ -13,11 +13,11 @@ export default (appInfo: EggAppInfo) => {
 
   config.orm = {
     type: 'mysql',
-    host: 'localhost',
+    host: '101.34.41.251',
     port: 3306,
-    username: 'root',
-    password: '123123',
-    database: 'cool',
+    username: 'cool',
+    password: 'd6892fa4853c0537',
+    database: 'machine',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
     // 打印日志
--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,3 +1,8 @@
+/*
+ * @Author: Chenxu
+ * @Date: 2021-03-24 20:02:20
+ * @LastEditTime: 2021-06-20 00:21:19
+ */
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
 
 export type DefaultConfig = PowerPartial<EggAppConfig>;
@@ -9,9 +14,9 @@ export default (appInfo: EggAppInfo) => {
     type: 'mysql',
     host: process.env.MYSQL_HOST || '127.0.0.1',
     port: parseInt(process.env.MYSQL_PORT || '3306'),
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || '123123',
-    database: process.env.MYSQL_DATABASE || 'cool',
+    username: process.env.MYSQL_USER || 'cool',
+    password: process.env.MYSQL_PASSWORD || 'd6892fa4853c0537',
+    database: process.env.MYSQL_DATABASE || 'machine',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: false,
     // 打印日志
